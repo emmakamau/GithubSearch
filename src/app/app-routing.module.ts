4e1dbe34d2119457a1b1bc2805318d57b18e23b1
EmmaK <emmaculatewkamau@gmail.com>
2022-04-18 08:15:07 +0300

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from './components/user/user.component';
+import { RepoComponent } from './components/repo/repo.component';
 
 const routes: Routes = [
-  {path:'',component:UserComponent}
+  {path:'',component:UserComponent},
+  {path:'repos',component:RepoComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { UserComponent } from './components/user/user.component';
+import { RepoComponent } from './components/repo/repo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    UserComponent
+    UserComponent,
+    RepoComponent
   ],
   imports: [
     BrowserModule,
